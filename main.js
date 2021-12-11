4e22f194f1d501c32f94c32f898b66e564b95adf
Marclap <jhona.cb991128@gmail.com>
2021-12-11 03:51:31 -0500

    Fix error equal

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
 const data = () => {
-	return document.getElementById('textview')
+    return document.getElementById('textview')
 }
 
 const insert = (num) => {
-	let form = data()
-	form.value = form.value + num
+    let form = data()
+    form.value = form.value + num
 }
 
 const equal = () => {
-	let form = data()
-	let data = form.value
-	if (data) form.value = eval(data)
+    let form = data()
+    let info = form.value
+    if (info) form.value = eval(info)
 }
 
 const clean = () => {
-	let form = data()
-	form.value = ''
+    let form = data()
+    form.value = ''
 }
 
 const back = () => {
-	let form = data()
-	form.value = form.value.substring(0, form.value.length - 1)
+    let form = data()
+    form.value = form.value.substring(0, form.value.length - 1)
 }
